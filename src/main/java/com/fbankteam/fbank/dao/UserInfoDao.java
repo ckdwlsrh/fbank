708700eb1451ff2cb264edf0eb53176e27397760
ju1645 <ju1645@naver.com>
2025-05-22 17:28:49 +0900

    [feat] 사용자 계정 및 정보 클래스 구현, DAO예외 처리 삭제

--- src/main/java/com/fbankteam/fbank/dao/UserInfoDao.java
+++ src/main/java/com/fbankteam/fbank/dao/UserInfoDao.java
@@ -1,17 +1,13 @@
 package com.fbankteam.fbank.dao;
-import com.fbankteam.fbank.domain.UserInfoVO;
 
-import java.sql.SQLException;
+import com.fbankteam.fbank.domain.UserInfoVO;
 import java.util.List;
 import java.util.Optional;
 
-
 public interface UserInfoDao {
-    int create(UserInfoVO info) throws SQLException;
-    List<UserInfoVO> getList() throws SQLException;
-    Optional<UserInfoVO> get(int id) throws SQLException;
-    int update(UserInfoVO info) throws SQLException;
-    int delete(int id) throws SQLException;
-
-
+    int create(UserInfoVO info);
+    List<UserInfoVO> getList();
+    Optional<UserInfoVO> get(int id);
+    int update(UserInfoVO info);
+    int delete(int id);
 }
--- src/main/java/com/fbankteam/fbank/domain/UserInfoVO.java
+++ src/main/java/com/fbankteam/fbank/domain/UserInfoVO.java
@@ -2,10 +2,13 @@ package com.fbankteam.fbank.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Builder;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
+
 public class UserInfoVO {
     private int id;
     private String userName;
