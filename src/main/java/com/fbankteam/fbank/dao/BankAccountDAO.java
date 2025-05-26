eda1cab401b3d458a23e1c98e0d9f7fdd3cf2a75
leehyemie <hyexxxmi@gmail.com>
2025-05-26 12:08:38 +0900

    [Feat] Bank Account VO, DAO 추가

---
+++ src/main/java/com/fbankteam/fbank/dao/BankAccountDAO.java
@@ -0,0 +1,25 @@
+package com.fbankteam.fbank.dao;
+
+import com.fbankteam.fbank.domain.BankAccountVO;
+
+import java.util.List;
+
+public interface BankAccountDAO {
+    // 계좌 등록
+    int insertBankAccount(BankAccountVO bankAccount);
+
+    // 계좌번호로 회원 정보 id 조회
+    int findUserIdByAccountNumber(String accountNumber);
+
+    // 최근 5개 계좌 조회
+    List<BankAccountVO> findTop5AccountsByUserId(int userId);
+
+    // 내 계좌 전체 조회
+    List<BankAccountVO> findAccountsByUserId(int userId);
+
+    // 즐겨찾기 계좌 목록 조회
+    List<BankAccountVO> findFavoriteAccountsByUserId(int userId);
+
+    // 즐겨찾기 토글
+    int toggleFavorite(int accountId, int userId);
+}
