08e03f2e2ba8e8a4d7b2d1581e0aac3cf8ed1fa0
leehyemie <hyexxxmi@gmail.com>
2025-06-10 16:46:53 +0900

    [Feat] Bank Account impl, test

--- src/main/java/com/fbankteam/fbank/domain/BankAccountVO.java
+++ src/main/java/com/fbankteam/fbank/domain/BankAccountVO.java
@@ -6,12 +6,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class BankAccountVO {
     private Integer id;
     private Integer userInfoId;
@@ -20,8 +19,5 @@ public class BankAccountVO {
     private Integer balance;
     private AccountType accountType; //'SAVINGS' or 'INSTALLMENT'
     private LocalDateTime createAt;
-    private boolean isFavorite;
-
-    // 1:N긴 하지만 5건 조인은 1:1이 더 편함
-    private AccountTransactionsVO transaction;
+    private Boolean isFavorite;
 }
